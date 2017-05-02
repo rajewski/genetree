10454bccd1a316a1e30c8d9682e4d3838ff8579d
rajewski <rajewski23@gmail.com>
2017-05-02 13:47:41 -0700

    add back script

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 This is the git repo for assembling several transcriptomes from 2x75bp NextSeq data
 
 By Alex Rajewski, UC, Riverside, 2017
+
+The input data is demultiplexed fastq files, so the barcodes have already been trimmed off, but not other quality trimming has taken place.
+
+This will also be a de novo assembly, so there is no genome assembly to reference.
--- trinity.sh
+++ trinity.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -l
-#SBATCH --ntasks=64
+#SBATCH --ntasks=16
 #SBATCH --nodes=1
 
+cd /rhome/arajewski/shared/genetree/analysis/
+
+module unload perl
 module load perl/5.22.0
 module load trinity-rnaseq
 
-Trinity --seqType fq --max_memory=50G --left flowcell608_lane1_pair1_ACAGTG.fastq.gz --right flowcell608_lane1_pair2_ACAGTG.fastq.gz --trimmomatic --CPU 64 --verbose ../trinityoutput/Fabiana
+Trinity --seqType fq --SS_lib_type RF --max_memory=50G --left /rhome/arajewski/shared/genetree/sequencing/flowcell608_lane1_pair1_ACAGTG.fastq.gz --right /rhome/arajewski/shared/genetree/sequencing/flowcell608_lane1_pair2_ACAGTG.fastq.gz --trimmomatic --CPU 16 --verbose --output /rhome/arajewski/shared/genetree/trinity_output_fabiana
