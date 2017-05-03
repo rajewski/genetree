07baccb7cda38893b2f26c4fc0213cc82081e93c
rajewski <rajewski23@gmail.com>
2017-05-02 18:23:20 -0700

    add symlinks

--- trinity.sh
+++ trinity.sh
@@ -3,9 +3,11 @@
 #SBATCH --nodes=1
 
 cd /rhome/arajewski/shared/genetree/analysis/
+ln -s /rhome/arajewski/shared/genetree/sequencing/flowcell608_lane1_pair1_ACAGTG.fastq.gz reads1.fastq.gz
+ln -s /rhome/arajewski/shared/genetree/sequencing/flowcell608_lane1_pair2_ACAGTG.fastq.gz reads2.fastq.gz
 
 module unload perl
 module load perl/5.22.0
 module load trinity-rnaseq
 
-Trinity --seqType fq --SS_lib_type RF --max_memory=50G --left /rhome/arajewski/shared/genetree/sequencing/flowcell608_lane1_pair1_ACAGTG.fastq.gz --right /rhome/arajewski/shared/genetree/sequencing/flowcell608_lane1_pair2_ACAGTG.fastq.gz --trimmomatic --CPU 16 --output /rhome/arajewski/shared/genetree/trinity_output_fabiana
+Trinity --seqType fq --max_memory 50G --left reads1.fastq.gz --right reads2.fastq.gz --trimmomatic --SS_lib_type RF --CPU 16 --output /rhome/arajewski/shared/genetree/trinity_output_fabiana
