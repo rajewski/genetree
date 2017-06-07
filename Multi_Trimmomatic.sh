5ab6db6d19d055709d0beb883c12e00a600bb05a
rajewski <rajewski23@gmail.com>
2017-06-07 16:40:18 -0700

    multi_trimmomatic handles both PE and SE

--- Multi_Trimmomatic.sh
+++ Multi_Trimmomatic.sh
@@ -6,22 +6,54 @@
 #SBATCH --mail-user=araje002@ucr.edu
 #SBATCH --mail-type=ALL
 
+PEfile1="flowcell608/flowcell608_lane1_pair1_ACAGTG.fastq.gz"
+PEfile2="flowcell608/flowcell608_lane1_pair1_ACAGTG.fastq.gz"
+SEfile="flowcell636/flowcell636_lane1_pair1_ACAGTG.fastq.gz"
+
 cd /rhome/arajewski/shared/genetree/analysis/
 
-echo "Removing previous links and creating new ones"
-rm -f PEreads*
-rm -f SEreads*
+#Check the input files exist
+if [ ! -e $PEfile1] || [ ! -e $PEfile2 ] || [ ! -e $SEfile]
+then
+    echo "$0: One of more inputs do not exist. Please check the filenames."
+    exit 1
+else 
+    echo "$0: Input files successfully located."
+    echo "$0: Removing old symlinks, just to be safe."
+    find -type l -delete
+fi
 
-ln -s /rhome/arajewski/shared/genetree/sequencing/flowcell608/flowcell608_lane1_pair1_ACAGTC.fastq.gz PEreads1.fastq.gz
-ln -s /rhome/arajewski/shared/genetree/sequencing/flowcell608/flowcell608_lane1_pair2_ACAGTG.fastq.gz PEreads2.fastq.gz
-ln -s /rhome/arajewski/shared/genetree/sequencing/flowcell636/flowcell636_lane1_pair1_ACAGTG.fastq.gz SEreads.fastq.gz
-
+#Load required packages
 echo "Loading required packages"
 module unload perl
 module load perl/5.22.0
 module load trinity-rnaseq
 
-echo "beginning single end sequence trimming with $SLURM_NTASKS cores."
-#Single End Trimming
+#Check for previous runs and do single end trimming
+if [ ! -e SEreadsTrimmed.gz.gz]
+then
+    echo "$0: Trimming Single-end read files with Trimmomatic using $SLURM_NTASKS cores."
+    echo "$0: Creating symlinks to datafiles."
+    ln -s /rhome/arajewski/shared/genetree/sequencing/$SEfile SEreads.fastq.gz
+    echo "$0: Running Trimmomatic."
+    java -jar /bigdata/bioinfo/pkgadmin/opt/linux/centos/7.x/x86_64/pkgs/trinity-rnaseq/2.4.0/trinity-plugins/Trimmomatic/trimmomatic.jar SE -threads $SLURM_NTASKS -phred33 SEreads.fastq.gz SEreadsTrimmed.fq.gz  ILLUMINACLIP:/bigdata/bioinfo/pkgadmin/opt/linux/centos/7.x/x86_64/pkgs/trinity-rnaseq/2.4.0/trinity-plugins/Trimmomatic/adapters/TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:4:5 LEADING:5 TRAILING:5 MINLEN:25
+    find -type l -delete
+    echo "$0: Done."
+else
+    echo "Trimmed Single-End Read File exists, skipping this step."
+fi
 
-java -jar /bigdata/bioinfo/pkgadmin/opt/linux/centos/7.x/x86_64/pkgs/trinity-rnaseq/2.4.0/trinity-plugins/Trimmomatic/trimmomatic.jar SE -threads $SLURM_NTASKS -phred33 SEreads.fastq.gz SEreadsTrimmed.fq.gz  ILLUMINACLIP:/bigdata/bioinfo/pkgadmin/opt/linux/centos/7.x/x86_64/pkgs/trinity-rnaseq/2.4.0/trinity-plugins/Trimmomatic/adapters/TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:4:5 LEADING:5 TRAILING:5 MINLEN:25
+#Check for previous run and do paired end trimming
+if [ ! -e PEreadsTrimmed_1P.fq.gz ] || [ ! -e PEreadsTrimmed_2P.fq.gz ] || [ ! -e PEreadsTrimmed_1U.fq.gz] || [ ! -e PEreadsTrimmed_2U.fq.gz]
+    echo "$0: Trimming paired end read files with Trimmomatic using $SLURM_NTASKS cores."
+    echo "$0: Creating symlinks to datafiles."
+    ln -s /rhome/arajewski/shared/genetree/sequencing/$PEfile1 PEreads1.fastq.gz
+    ln -s /rhome/arajewski/shared/genetree/sequencing/$PEfile2 PEreads2.fastq.gz
+    echo "$0: Running Trimmomatic."
+    java -jar /bigdata/bioinfo/pkgadmin/opt/linux/centos/7.x/x86_64/pkgs/trinity-rnaseq/2.4.0/trinity-plugins/Trimmomatic/trimmomatic.jar PE -threads $SLURM_NTASKS -phred33 PEreads1.fastq.gz PEreads2.fastq.gz -baseout PEreadsTrimmed.fq.gz  ILLUMINACLIP:/bigdata/bioinfo/pkgadmin/opt/linux/centos/7.x/x86_64/pkgs/trinity-rnaseq/2.4.0/trinity-plugins/Trimmomatic/adapters/TruSeq3-SE.fa:2:30:10 SLIDINGWINDOW:4:5 LEADING:5 TRAILING:5 MINLEN:25
+    find -type l -delete
+    echo "$0: Done."
+else
+    echo "Trimmed paired-end read files exist, skipping this step."
+fi
+
