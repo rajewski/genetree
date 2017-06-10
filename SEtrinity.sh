a32b17b4703080548a3aab20653ef85d6688e19e
rajewski <rajewski23@gmail.com>
2017-06-10 09:55:00 -0700

    add normalization to SE script

--- SEtrinity.sh
+++ SEtrinity.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH --ntasks=16
 #SBATCH --nodes=1
-#SBATCH --mem=100G
-#SBATCH --time=06:00:00
+#SBATCH --mem=400G
+#SBATCH --time=10:00:00
 #SBATCH --mail-user=araje002@ucr.edu
 #SBATCH --mail-type=ALL
 
@@ -12,4 +12,4 @@ module unload perl
 module load perl/5.22.0
 module load trinity-rnaseq
 
-Trinity --seqType fq --max_memory $((SLURM_MEM_PER_NODE/1000))'G' --bflyCalculateCPU --single SEreadsTrimmed.fq.gz --SS_lib_type R --CPU $SLURM_NTASKS --full_cleanup --output /rhome/arajewski/shared/genetree/trinity_output_fabianaSEonly
+Trinity --seqType fq --max_memory $((SLURM_MEM_PER_NODE/1000))'G' --bflyCalculateCPU --single SEreadsTrimmed.fq.gz --SS_lib_type R --CPU $SLURM_NTASKS --full_cleanup --normalize_reads --output /rhome/arajewski/shared/genetree/trinity_output_fabianaSEonly2
